76730fdb9bb1b66816a235d852ab848181932738
rutuja3562 <rspatil3562@gmail.com>
2024-09-21 18:14:20 +0530

    balanced parentheses done

---
+++ dsa-practice/most-asked-questions/checkIsParenthes.js
@@ -0,0 +1,24 @@
+function checkIsParentheses(string) {
+  var stack = [];
+  var n = string.length;
+  for (var i = 0; i < n; i++) {
+    if (string[i] === "(" || string[i] === "{" || string[i] === "[") {
+      stack.push(string[i]);
+    } else {
+      if (
+        (string[i] == ")" && stack[stack.length - 1] === "(") ||
+        (string[i] == "}" && stack[stack.length - 1] === "{") ||
+        (string[i] == "]" && stack[stack.length - 1] === "[")
+      ) {
+        stack.pop();
+      }
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+var string = "{[({}){]}";
+console.log("is balanced", checkIsParentheses(string));
