2a3fb90d1726ce06162a81bb128074dd8f835982
rutuja3562 <rspatil3562@gmail.com>
2024-10-01 14:39:47 +0530

    optimize solution

--- dsa-practice/most-asked-questions/rotateArray.js
+++ dsa-practice/most-asked-questions/rotateArray.js
@@ -9,6 +9,35 @@ function rotateArray(arr, k) {
   }
   return rotatedArr;
 }
+
+// Optimized method
+// Time Complexity: O(N)
+// Space Complexity:O(1)
+
+function rotateArrayByK(k, arr) {
+  var n = arr.length;
+  k = k % n;
+  reverseArr(arr, 0, k - 1);
+  reverseArr(arr, k, n - 1);
+  reverseArr(arr, 0, n - 1);
+  return arr;
+}
+
+function reverseArr(arr, i, j) {
+  while (i < j) {
+    swap(arr, i, j);
+    i++;
+    j--;
+  }
+}
+
+function swap(a, i, j) {
+  var temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+
 var arr = [1, 2, 3, 4, 5, 6, 7];
 var k = 2;
 console.log(rotateArray(arr, k));
+console.log(rotateArrayByK(k, arr));
