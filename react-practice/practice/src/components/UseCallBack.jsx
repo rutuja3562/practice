a2187f148f30bebc06d78f3b1a4409c75f1feb04
rutuja3562 <rspatil3562@gmail.com>
2024-10-09 15:02:56 +0530

    practice

--- react-practice/practice/src/components/UseCallBack.jsx
+++ react-practice/practice/src/components/UseCallBack.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 export const UseCallBack = () => {
-  return <div>UseCallBack</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+    console.log("Button clicked!");
+  }, []);
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <ButtonComponent handleClick={handleClick} />
+    </div>
+  );
+};
+
+const ButtonComponent = ({ handleClick }) => {
+  return <button onClick={handleClick}>Click</button>;
 };
